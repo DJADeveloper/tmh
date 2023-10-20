872297f794599a165d2bbd898795d2063616b8bd
DJADeveloper <63814360+DJA093@users.noreply.github.com>
2023-10-20 06:32:24 -0400

    Remove Unwanted Component - SoftareService

--- src/screens/Services/index.js
+++ src/screens/Services/index.js
@@ -1,11 +1,10 @@
 import React from "react";
 
 import Catalog from "../Class01/Catalog";
-import SoftwareService from "./Class02Details copy/ServicePage";
 
 const ServiceComponent = () => {
   <>
-    <SoftwareService />
+    {/* <SoftwareService /> */}
     <Catalog />
   </>;
 };
