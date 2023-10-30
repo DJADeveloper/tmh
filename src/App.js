581993d4a51272c1247e137d54226e7f22d4cf55
DJADeveloper <63814360+DJA093@users.noreply.github.com>
2023-10-30 11:59:41 -0400

    Add Scroll to Top Feature

--- src/App.js
+++ src/App.js
@@ -30,270 +30,308 @@ import SMAService from "./screens/Services/DigitalMarketing/SocialMediaAds";
 import SEOService from "./screens/Services/DigitalMarketing/SEO";
 import EmailMarketingService from "./screens/Services/DigitalMarketing/EmailMarketing";
 import MobibleDevelopmentService from "./screens/Services/SoftwareDevelopment/MobilDevelopment";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import ScrollButton from "./components/ScrollButton";
 
 function App() {
   // Function to initialize Tawk.to
-  const initTawkTo = () => {
-    // Check if Tawk_API is already defined, if not, define it
-    if (!window.Tawk_API) {
-      window.Tawk_API = {};
-      window.Tawk_LoadStart = new Date();
+  // const initTawkTo = () => {
+  //   // Check if Tawk_API is already defined, if not, define it
+  //   if (!window.Tawk_API) {
+  //     window.Tawk_API = {};
+  //     window.Tawk_LoadStart = new Date();
 
-      const s1 = document.createElement("script"),
-        s0 = document.getElementsByTagName("script")[0];
+  //     const s1 = document.createElement("script"),
+  //       s0 = document.getElementsByTagName("script")[0];
 
-      s1.async = true;
-      s1.src = "https://embed.tawk.to/6534fef0f2439e1631e6fc6b/1hdbg7mvn";
-      s1.charset = "UTF-8";
-      s1.setAttribute("crossorigin", "*");
+  //     s1.async = true;
+  //     s1.src = "https://embed.tawk.to/6534fef0f2439e1631e6fc6b/1hdbg7mvn";
+  //     s1.charset = "UTF-8";
+  //     s1.setAttribute("crossorigin", "*");
 
-      s0.parentNode.insertBefore(s1, s0);
-    }
+  //     s0.parentNode.insertBefore(s1, s0);
+  //   }
+  // };
+  const [showButton, setShowButton] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      // Here, 10 is an arbitrary number to provide a slight buffer.
+      // Adjust as needed.
+      if (window.scrollY > 500) {
+        setShowButton(true);
+      } else {
+        setShowButton(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up the event listener when the component is unmounted.
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const handleScrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
   };
+  // Function to initialize Tawk.to
 
   // Chatbot initialization
   useEffect(() => {
-    initTawkTo();
+    // initTawkTo();
   }, []);
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <Page>
-            <Home />
-          </Page>
-        }
-      />
-      <Route
-        path="/features"
-        element={
-          <Page>
-            <Features />
-          </Page>
-        }
-      />
-      <Route
-        path="/pricing"
-        element={
-          <Page>
-            <Pricing />
-          </Page>
-        }
-      />
-      <Route
-        path="/contact"
-        element={
-          <Page>
-            <Download />
-          </Page>
-        }
-      />
-      <Route
-        path="/services"
-        element={
-          <Page>
-            <Class01 />
-          </Page>
-        }
-      />
-      <Route
-        path="/class01-details"
-        element={
-          <Page>
-            <Class01Details />
-          </Page>
-        }
-      />
-      <Route
-        path="/about"
-        element={
-          <Page>
-            <Class02 />
-          </Page>
-        }
-      />
-      <Route
-        path="/work"
-        element={
-          <Page>
-            <Class02Details />
-          </Page>
-        }
-      />
-      <Route
-        path="/lifestyle"
-        element={
-          <Page>
-            <Lifestyle />
-          </Page>
-        }
-      />
-      <Route
-        path="/article"
-        element={
-          <Page>
-            <Article />
-          </Page>
-        }
-      />
-      {/* SERVICES */}
-      <Route
-        path="/service-frontend"
-        element={
-          <Page>
-            <FrontendService service="frontend" />
-          </Page>
-        }
-      />
-      <Route
-        path="/service-backend"
-        element={
-          <Page>
-            <BackendService service="backend" />
-          </Page>
-        }
-      />
-      <Route
-        path="/service-ecommerce"
-        element={
-          <Page>
-            <EcommerceService service="ecommerce" />
-          </Page>
-        }
-      />
-      <Route
-        path="/service-mobile"
-        element={
-          <Page>
-            <MobibleDevelopmentService service="mobile" />
-          </Page>
-        }
-      />
-      <Route
-        path="/service-cms"
-        element={
-          <Page>
-            <CMSService service="cms" />
-          </Page>
-        }
-      />
-      <Route
-        path="/service-pwa"
-        element={
-          <Page>
-            <PWAService service="pwa" />
-          </Page>
-        }
-      />
-      <Route
-        path="/service-web-maintenance"
-        element={
-          <Page>
-            <WebMaintenanceService service="web-maintenance" />
-          </Page>
-        }
-      />
-      <Route
-        path="/service-chatbot"
-        element={
-          <Page>
-            <ChatBotService service="chatbot" />
-          </Page>
-        }
-      />
-      <Route
-        path="/service-recommendation-systems"
-        element={
-          <Page>
-            <RecommendationSystemsService service="recommended" />
-          </Page>
-        }
-      />
-      <Route
-        path="/service-machine-learning"
-        element={
-          <Page>
-            <MachineLearningService service="ml" />
-          </Page>
-        }
-      />
-      <Route
-        path="/service-natural-language-processing"
-        element={
-          <Page>
-            <NLPService service="nlp" />
-          </Page>
-        }
-      />
-      <Route
-        path="/service-computer-vision"
-        element={
-          <Page>
-            <ComputerVisionService service="computer-vision" />
-          </Page>
-        }
-      />
-      <Route
-        path="/service-process-automation"
-        element={
-          <Page>
-            <ProcessAutomationService service="process-automation" />
-          </Page>
-        }
-      />
-      <Route
-        path="/service-story-branding"
-        element={
-          <Page>
-            <StoryBrandingService service="storybranding" />
-          </Page>
-        }
-      />
-      <Route
-        path="/service-content-creation"
-        element={
-          <Page>
-            <ContentCreationService service="content-creation" />
-          </Page>
-        }
-      />
-      <Route
-        path="/service-social-media-marketing"
-        element={
-          <Page>
-            <SMMService service="social-media-marketing" />
-          </Page>
-        }
-      />
-      <Route
-        path="/service-social-media-ads"
-        element={
-          <Page>
-            <SMAService service="social-ads" />
-          </Page>
-        }
-      />
-      <Route
-        path="/service-search-engine-optimization"
-        element={
-          <Page>
-            <SEOService service="seo" />
-          </Page>
-        }
-      />
-      <Route
-        path="/service-email-marketing"
-        element={
-          <Page>
-            <EmailMarketingService service="email-marketing" />
-          </Page>
-        }
-      />
+    <>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Page>
+              <Home />
+            </Page>
+          }
+        />
+        <Route
+          path="/features"
+          element={
+            <Page>
+              <Features />
+            </Page>
+          }
+        />
+        <Route
+          path="/pricing"
+          element={
+            <Page>
+              <Pricing />
+            </Page>
+          }
+        />
+        <Route
+          path="/contact"
+          element={
+            <Page>
+              <Download />
+            </Page>
+          }
+        />
+        <Route
+          path="/services"
+          element={
+            <Page>
+              <Class01 />
+            </Page>
+          }
+        />
+        <Route
+          path="/class01-details"
+          element={
+            <Page>
+              <Class01Details />
+            </Page>
+          }
+        />
+        <Route
+          path="/about"
+          element={
+            <Page>
+              <Class02 />
+            </Page>
+          }
+        />
+        <Route
+          path="/work"
+          element={
+            <Page>
+              <Class02Details />
+            </Page>
+          }
+        />
+        <Route
+          path="/lifestyle"
+          element={
+            <Page>
+              <Lifestyle />
+            </Page>
+          }
+        />
+        <Route
+          path="/article"
+          element={
+            <Page>
+              <Article />
+            </Page>
+          }
+        />
+        {/* SERVICES */}
+        <Route
+          path="/service-frontend"
+          element={
+            <Page>
+              <FrontendService service="frontend" />
+            </Page>
+          }
+        />
+        <Route
+          path="/service-backend"
+          element={
+            <Page>
+              <BackendService service="backend" />
+            </Page>
+          }
+        />
+        <Route
+          path="/service-ecommerce"
+          element={
+            <Page>
+              <EcommerceService service="ecommerce" />
+            </Page>
+          }
+        />
+        <Route
+          path="/service-mobile"
+          element={
+            <Page>
+              <MobibleDevelopmentService service="mobile" />
+            </Page>
+          }
+        />
+        <Route
+          path="/service-cms"
+          element={
+            <Page>
+              <CMSService service="cms" />
+            </Page>
+          }
+        />
+        <Route
+          path="/service-pwa"
+          element={
+            <Page>
+              <PWAService service="pwa" />
+            </Page>
+          }
+        />
+        <Route
+          path="/service-web-maintenance"
+          element={
+            <Page>
+              <WebMaintenanceService service="web-maintenance" />
+            </Page>
+          }
+        />
+        <Route
+          path="/service-chatbot"
+          element={
+            <Page>
+              <ChatBotService service="chatbot" />
+            </Page>
+          }
+        />
+        <Route
+          path="/service-recommendation-systems"
+          element={
+            <Page>
+              <RecommendationSystemsService service="recommended" />
+            </Page>
+          }
+        />
+        <Route
+          path="/service-machine-learning"
+          element={
+            <Page>
+              <MachineLearningService service="ml" />
+            </Page>
+          }
+        />
+        <Route
+          path="/service-natural-language-processing"
+          element={
+            <Page>
+              <NLPService service="nlp" />
+            </Page>
+          }
+        />
+        <Route
+          path="/service-computer-vision"
+          element={
+            <Page>
+              <ComputerVisionService service="computer-vision" />
+            </Page>
+          }
+        />
+        <Route
+          path="/service-process-automation"
+          element={
+            <Page>
+              <ProcessAutomationService service="process-automation" />
+            </Page>
+          }
+        />
+        <Route
+          path="/service-story-branding"
+          element={
+            <Page>
+              <StoryBrandingService service="storybranding" />
+            </Page>
+          }
+        />
+        <Route
+          path="/service-content-creation"
+          element={
+            <Page>
+              <ContentCreationService service="content-creation" />
+            </Page>
+          }
+        />
+        <Route
+          path="/service-social-media-marketing"
+          element={
+            <Page>
+              <SMMService service="social-media-marketing" />
+            </Page>
+          }
+        />
+        <Route
+          path="/service-social-media-ads"
+          element={
+            <Page>
+              <SMAService service="social-ads" />
+            </Page>
+          }
+        />
+        <Route
+          path="/service-search-engine-optimization"
+          element={
+            <Page>
+              <SEOService service="seo" />
+            </Page>
+          }
+        />
+        <Route
+          path="/service-email-marketing"
+          element={
+            <Page>
+              <EmailMarketingService service="email-marketing" />
+            </Page>
+          }
+        />
 
-      <Route path="/class/:serviceId" element={<Class01Details />} />
-    </Routes>
+        <Route path="/class/:serviceId" element={<Class01Details />} />
+      </Routes>
+      {showButton && (
+        <ScrollButton
+          onScroll={handleScrollToTop}
+          arrowDirection="up"
+          className="scroll-btn"
+        />
+      )}
+    </>
   );
 }
 
--- src/components/ScrollButton/index.js
+++ src/components/ScrollButton/index.js
@@ -3,12 +3,12 @@ import cn from "classnames";
 import styles from "./ScrollButton.module.sass";
 import Icon from "../Icon";
 
-const ScrollButton = ({ className, onScroll }) => {
+const ScrollButton = ({ className, onScroll, arrowDirection }) => {
   return (
     <button className={cn(styles.scroll, className)} onClick={onScroll}>
       <div className={styles.line}>
-        <Icon name="arrow-down" size="14" />
-        <Icon name="arrow-down" size="14" />
+        <Icon name={`arrow-${arrowDirection}`} size="14" />
+        <Icon name={`arrow-${arrowDirection}`} size="14" />
       </div>
     </button>
   );
