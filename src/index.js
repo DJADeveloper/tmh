76821a9dfa171857417b9927a1836c1387d535a2
DJADeveloper <63814360+DJA093@users.noreply.github.com>
2023-11-20 15:54:28 -0500

    HashRouter Change

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import App from "./App";
 import ServicesProvider from "./context/Service/ServicesProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ServicesProvider>
         <App />
       </ServicesProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
